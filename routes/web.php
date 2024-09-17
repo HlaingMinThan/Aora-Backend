78805ef158176f09ce1988f0bc7d570f81dd2756
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-09-17 16:51:31 +0700

    expo push noti test api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test', function () {
+    $expoPushToken = "ExponentPushToken[S2xWkaHhkS9s9qAV-eq7dF]";
+    $message = [
+        "to" => $expoPushToken, //hard code expo token now
+        "title" => "Hlaing min than",
+        "body" => "Min ga lar par",
+        "data" => [
+            "screen" => "/signin"
+        ]
+    ];
+    $url = 'https://exp.host/--/api/v2/push/send';
+
+    $response = Http::post($url, [
+        'to' => $expoPushToken,
+        'title' => $message['title'],
+        'body' => $message['body'],
+        'data' => $message['data'],
+    ]);
+
+    return $response->json();
 });
